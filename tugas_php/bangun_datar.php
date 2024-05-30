9096631eb941a1e99f1592860f705b69fe0eeaf2
Okto16 <bagastx@gmail.com>
2024-05-30 13:59:38 +0700

    Tugas Php Operator

---
+++ tugas_php/bangun_datar.php
@@ -0,0 +1,34 @@
+<?php
+// Luas Persegi
+$sisi = 2;
+$luas = $sisi * $sisi;
+echo "Luas Persegi = $luas";
+echo "<br>";
+
+//Segitiga
+$alas = 3;
+$tinggi = 3;
+$luas = $alas * $tinggi / 2;
+echo "Luas Segitiga = $luas";
+echo "<br>";
+
+//Lingkaran
+$phi = 3.14;
+$r = 7;
+$luas = $phi * $r ** 2;
+echo "Luas Lingkaran = $luas";
+echo "<br>";
+
+//Trapesium
+$t = 5;
+$a = 3;
+$b = 4;
+$luas = ($a + $b) / 2 * $t;
+echo "Luas Trapesium = $luas";
+echo "<br>";
+
+//Jajar Genjang
+$alas = 3;
+$tinggi = 4;
+$luas = $alas * $tinggi;
+echo "Luas Jajar Genjang = $luas";
---
+++ tugas_php/bangun_ruang.php
@@ -0,0 +1,20 @@
+<?php
+//Kubus
+$r = 4;
+$v = $r ** 3;
+echo "Volume kubus = $v";
+echo "<br>";
+
+//Balok
+$panjang = 3;
+$lebar = 2;
+$tinggi = 8;
+$v = $panjang * $lebar * $tinggi;
+echo "Volume balok = $v";
+echo "<br>";
+
+//Bola
+$phi = 3.14;
+$jari = 5;
+$v = 4 / 3 * $phi * $jari ** 3;
+echo "Volume bola = $v";
